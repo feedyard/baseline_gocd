2582e0af7d47e2c87b19b3735cdc798f9a73f219
Nic Cheneweth <nchenewe@thoughtworks.com>
2017-06-08 21:57:01 -0500

    circleci testing

--- tasks.py
+++ tasks.py
@@ -11,3 +11,13 @@ def enckeys(ctx):
     ctx.run("openssl aes-256-cbc -e -in cert.pem -out cert.ci -k $KEY", pty=True)
     ctx.run("openssl aes-256-cbc -e -in key.pem -out key.ci -k $KEY", pty=True)
     ctx.run("openssl aes-256-cbc -e -in secrets.env -out secrets.ci -k $KEY", pty=True)
+
+
+
+
+def docker_remote(tfvars, host_name):
+    key_path = expanduser("~") + '/.docker/machine/machines/{}/'.format(tfvars['bootstrap-instance-name'])
+return "docker --tlsverify --tlscacert={} --tlscert={} --tlskey={} -H={} ".format(key_path + 'ca.pem',
+                                                                                  key_path + 'cert.pem',
+                                                                                  key_path + 'key.pem',
+                                                                                  host_name + ':2376')
